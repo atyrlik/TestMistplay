de82d5a173994fd6ace1201192ba0856013e260a
Alexandre <alexandre.tyrlik@gmail.com>
2020-10-16 18:20:30 +0200

    Handle loading and error UI

--- app/src/main/java/me/atyrlik/testmistplay/gameslistscreen/view/GamesListFragment.kt
+++ app/src/main/java/me/atyrlik/testmistplay/gameslistscreen/view/GamesListFragment.kt
@@ -1,10 +1,11 @@
 package me.atyrlik.testmistplay.gameslistscreen.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import me.atyrlik.testmistplay.databinding.GamesListFragmentBinding
 import me.atyrlik.testmistplay.gameslistscreen.viewmodel.GamesListViewModel
@@ -38,6 +39,27 @@ class GamesListFragment : Fragment() {
             with (binding.gamesCategoriesList.adapter as GamesCategoriesAdapter) {
                 this.gamesCategories = gamesCategories
                 notifyDataSetChanged()
+            }
+        })
+
+        viewModel.loadingState.observe(viewLifecycleOwner, Observer { state ->
+            when (state) {
+                GamesListViewModel.LoadingState.LOADING -> {
+                    binding.gamesCategoriesList.isVisible = false
+                    binding.loadingSpinner.isVisible = true
+                    binding.errorMessage.isVisible = false
+                }
+                GamesListViewModel.LoadingState.READY -> {
+                    binding.gamesCategoriesList.isVisible = true
+                    binding.loadingSpinner.isVisible = false
+                    binding.errorMessage.isVisible = false
+                }
+                GamesListViewModel.LoadingState.ERROR, null -> {
+                    // In a real app, we should give an option to the user to retry.
+                    binding.gamesCategoriesList.isVisible = false
+                    binding.loadingSpinner.isVisible = false
+                    binding.errorMessage.isVisible = true
+                }
             }
         })
     }
--- app/src/main/java/me/atyrlik/testmistplay/gameslistscreen/viewmodel/GamesListViewModel.kt
+++ app/src/main/java/me/atyrlik/testmistplay/gameslistscreen/viewmodel/GamesListViewModel.kt
@@ -1,9 +1,6 @@
 package me.atyrlik.testmistplay.gameslistscreen.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import me.atyrlik.testmistplay.models.GamesList
 import me.atyrlik.testmistplay.repository.GamesRepository
 
@@ -11,10 +8,21 @@ class GamesListViewModel(
     private val repository: GamesRepository
 ) : ViewModel() {
 
+    enum class LoadingState {
+        LOADING, READY, ERROR
+    }
+
+    val loadingState = MutableLiveData<LoadingState>(LoadingState.LOADING)
+
     val games: LiveData<List<GamesList>> = liveData(context = viewModelScope.coroutineContext) {
         repository.getGames().fold(
-            onSuccess = { emit(it) },
-            onFailure = {} // Don't emit anything is there is an error. We should display something to the user.
+            onSuccess = {
+                emit(it)
+                loadingState.postValue(LoadingState.READY)
+            },
+            onFailure = {
+                loadingState.postValue(LoadingState.ERROR)
+            }
         )
     }
 
